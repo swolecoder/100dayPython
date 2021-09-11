aa2fe6b94ecd9281c2a336de348e1b2d27ed948e
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-09-11 11:39:49 -0400

    Time: 183 ms (28.09%), Space: 14.5 MB (90.96%) - LeetHub

---
+++ valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+        def valid(l, r):
+            
+            
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l +=1
+                r -=1
+                
+            return True
+        
+        
+        l = 0
+        r = len(s)-1
+        
+        while l < r:
+            
+            if s[l] != s[r]:
+                return valid(l+1,r) or valid(l, r-1)
+             
+            l +=1
+            r -=1
+        
+        return True
+        
+        
+        
