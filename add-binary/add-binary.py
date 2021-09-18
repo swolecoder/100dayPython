6735b2d7adb5df597e367c77e5bf25c691e4221e
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-09-17 21:36:36 -0400

    Time: 46 ms (18.71%), Space: 14.2 MB (79.41%) - LeetHub

--- add-binary/add-binary.py
+++ add-binary/add-binary.py
@@ -1,44 +1,23 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         
-        sum = ""
+        ans = ""
+        
+        a = list(a)
+        b = list(b)
         carry = 0
         
-        i, j = len(a)-1 ,len(b)-1
+        while a or b or carry ==1:
+            
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+            
+            ans = str(carry % 2) + ans
+            
+            carry = carry//2
         
         
-        while i >=0 or j >= 0:
-            
-            a1 = 0
-            b1 = 0
-            
-            if i >=0:
-                a1 = ord(a[i]) - 48
-                i -=1
-            if j >=0:
-                b1 = ord(b[j]) -48
-                j -=1
-            
-            currentSum = a1 + b1 + carry
-            
-            if currentSum >2:
-                sum = "1" + sum
-                print("Ashish")
-                carry = 1
-            elif currentSum == 2:
-                sum = "0" + sum
-                carry = 1
-            elif currentSum == 1:
-                sum = "1"+ sum
-                carry = 0
-            else:
-                sum = "0"+sum
-                carry = 0
-            
-            print("ashish",a1,b1, currentSum,carry)
-        print(sum, carry)
+        return ans
         
-        if carry == 1:
-            sum = "1"+ sum
-        return sum
-                
