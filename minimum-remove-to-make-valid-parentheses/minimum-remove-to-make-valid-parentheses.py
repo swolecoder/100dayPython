1f82a8852ddb4518c4227f3071ee6513ff180294
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-09-19 03:22:34 -0400

    Time: 156 ms (38.48%), Space: 17 MB (12.14%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -2,43 +2,35 @@ class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         
         data = list(s)
-        # print(data, 0 > 0)
         
-        count = 0
+        open = 0
         
-        for i, ch in enumerate(data):
+        for i in range(len(data)):
+            curr = data[i]
             
-            if ch == "(":
-                count +=1
-            elif ch == ")":
-                if count > 0:
-                    count -=1
-                else:
+            if curr == "(":
+                open +=1
+            elif curr == ")":
+                if not open:
                     data[i] = "#"
+                else:
+                    open -=1
         
+        open = 0
+        for i in range(len(data)-1,-1,-1):
+            curr = data[i]
+            
+            if curr == ")":
+                open +=1
+            elif curr == "(":
+                if not open:
+                    data[i] = "#"
+                else:
+                    open -=1
         print(data)
         
-        
-        count = 0
-        
-        for i in range(len(data)-1,-1,-1):
-            if data[i] == ")":
-                count +=1
-            elif data[i] == "(":
-                if count > 0:
-                    count -=1
-                else:
-                    data[i] = "#"
-            print(data[i],count)
-        
-        
-        ans = ""
-        
-        for i, ch in enumerate(data):
-            
-            if ch not in "#":
-                ans += ch
-                
-        return ans
-            
-        
+        def filterhelper(s):
+            return s[0] != "#"
+        a = filter(filterhelper, data)
+        # print(''.join(a))
+        return ''.join(a)
