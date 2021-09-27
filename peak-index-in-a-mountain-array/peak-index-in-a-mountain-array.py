189e8d575bdbe67a179709bb43561efffe70c414
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-09-27 18:03:04 -0400

    Time: 80 ms (39.71%), Space: 15.4 MB (57.00%) - LeetHub

---
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
+                return i
+        
+        return -1
+        
