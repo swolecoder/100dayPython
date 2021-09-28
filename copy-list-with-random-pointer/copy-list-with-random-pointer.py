4962cb52802e6200fe3b2a2ff335be902f629bec
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-09-27 20:03:18 -0400

    Time: 36 ms (72.61%), Space: 15.1 MB (44.29%) - LeetHub

--- copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -9,23 +9,25 @@ class Node:
 from collections import defaultdict
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
+        map = defaultdict(lambda:None)
         
-        map = defaultdict(lambda: None)
         
         curr = head
-        
+         
         while curr:
             map[curr] = Node(curr.val)
-            curr = curr.next
+            curr = curr.next 
         
-        print(map)
+        
         curr = head
         
         while curr:
-            dep = map[curr]
-            dep.next = map[curr.next]
-            dep.random = map[curr.random]
+            key = map[curr]
+            
+            key.next = map[curr.next]
+            key.random = map[curr.random]
             
             curr = curr.next
+        
         
         return map[head]
