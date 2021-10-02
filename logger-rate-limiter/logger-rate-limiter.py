2c7913bd219471922616c3ebf94e420e759ab5b5
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-10-02 14:21:52 -0400

    Time: 132 ms (98.34%), Space: 19.8 MB (84.18%) - LeetHub

--- logger-rate-limiter/logger-rate-limiter.py
+++ logger-rate-limiter/logger-rate-limiter.py
@@ -1,35 +1,33 @@
 class Logger:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         
-        self.dict = {}
+        self.map = {}
         
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        """
-        Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity.
-        """
         
-        if message not in self.dict:
-            self.dict[message] = timestamp
+        map = self.map
+        
+        if message not in map:
+            map[message] = timestamp
+            
             return True
         else:
             
-            # fidn last time stamp
-            val = self.dict.get(message)
-            print(val)
-            if (timestamp - (val + 10 )) >=0:
-                self.dict[message] = timestamp
+            #get val 
+            val = map[message]
+            
+            logic = val + 10 <= timestamp
+            
+            if logic:
+                map[message] = timestamp
                 return True
             else:
                 return False
                 
-        
+            
+            
 
 
 # Your Logger object will be instantiated and called as such:
