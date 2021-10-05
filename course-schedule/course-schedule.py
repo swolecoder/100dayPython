3988d7677ad44b608a7c0f2e17ed9770b87afcc0
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-10-04 20:40:20 -0400

    Time: 96 ms (81.38%), Space: 17.5 MB (15.95%) - LeetHub

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -1,46 +1,42 @@
-from collections import defaultdict, deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        indegres = [0] * numCourses
-        m = defaultdict(list)
+        map = { i: [] for i in range(numCourses)}
         
-        for i in range(len(prerequisites)):
-            a, b = prerequisites[i][0],prerequisites[i][1]
-            m[b].append(a)
-            indegres[a] +=1
+        print(map)
         
-        s = deque([])
         
-        for i in range(len(indegres)):
-            if indegres[i] == 0:
-                s.append(i)
+        for cource,pre in prerequisites:
+            map[pre].append(cource)
+            
+        
+        print(map)
+        
+        
+        
+        visited = set()
+        
+        def dfs(node):
+            if node in visited:
+                return False
+            if map[node] == []:
+                return True
+            
+            visited.add(node)
+            
+            for pre in map[node]:
                 
-        res = []
-        print(s, m)
-        while s:
-            data = s.popleft()
-            res.append(data)
+                if not dfs(pre):
+                    return False
             
-            if data in m:
-                for child in m[data]:
-                    print("Ass", child,indegres[child] )
-                    indegres[child] -=1
-                    
-                    if indegres[child] == 0:
-                        s.append(child)
-        
-        print(res)
-        
-        if len(res) == numCourses:
+            visited.remove(node)
+            map[node] = []
             return True
-        
-        return False
-                    
             
-        
-
-        
-        
-         
-        
+            
+        for i in range(numCourses):
+            if not dfs(i):return False
+            
+        return True
+            
+            
