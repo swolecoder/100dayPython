1fe749e73b000db379f8b224315f7620a33a089a
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-10-11 14:05:02 -0400

    Time: 99 ms (29.43%), Space: 18.2 MB (79.01%) - LeetHub

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -11,11 +11,10 @@ class Solution:
         curr = root
         
         while curr:
-            if p.val < curr.val and q.val < curr.val:
-                curr = curr.left
-            elif p.val > curr.val and q.val > curr.val:
+            if p.val > curr.val and q.val > curr.val:
                 curr = curr.right
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
             else:
                 return curr
-            
         
