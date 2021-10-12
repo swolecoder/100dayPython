8104858c34a057575a8aea72b8707737664cf2ef
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-10-11 23:03:13 -0400

    Time: 97 ms (8.35%), Space: 16.7 MB (5.72%) - LeetHub

--- diameter-of-binary-tree/diameter-of-binary-tree.py
+++ diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -5,22 +5,46 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def height(self,node):
-        if not node:
-            return 0
-        l = self.height(node.left)
-        r = self.height(node.right)
-        return max(l,r)+1
+    def  diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+        res = float('-inf')
+        def dfs(node):
+            nonlocal res
+            if not node:
+                #diamter and height
+                return [-1,-1]
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+            diamter = left[1] + right[1] + 2
+            
+            res = max(res,diamter)
+            height = 1 + max(left[1],right[1])
+            print([diamter, height])
+            return [diamter, height]
         
-        l = self.height(root.left)
-        r = self.height(root.right)
-        lDiameter = self.diameterOfBinaryTree(root.left)
-        rDiamter = self.diameterOfBinaryTree(root.right)
-        return max(l+r, max(lDiameter,rDiamter))
+        dfs(root)
+        
+        return res
+            
+            
+#         if not node:
+#             return 0
+#         l = self.height(node.left)
+#         r = self.height(node.right)
+#         return max(l,r)+1
+        
+#     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+        
+#         l = self.height(root.left)
+#         r = self.height(root.right)
+#         lDiameter = self.diameterOfBinaryTree(root.left)
+#         rDiamter = self.diameterOfBinaryTree(root.right)
+#         return max(l+r, max(lDiameter,rDiamter))
+
+         
         
     
     
